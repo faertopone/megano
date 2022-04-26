fe119d32ce386ca9a84e4abe2b776e685ee13388
sergey_klimov <s.klimov@bk.ru>
2022-04-26 10:45:35 +0700

    Добавлен пример приложения с m2m отношениями

---
+++ products/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Product(models.Model):
+    """
+    Модель товара
+    """
+    name = models.CharField(max_length=256)
+    property = models.ManyToManyField('Property', through='ProductProperty', null=True, blank=True)
+
+
+class Property(models.Model):
+    """
+    Модель измеримых свойств товара.
+    Поле measure можно определить через предопределенный список единиц измерения
+    """
+    name = models.CharField(max_length=256)
+    measure = models.CharField(max_length=256, verbose_name='Единица измерения')
+
+
+class ProductProperty(models.Model):
+    """
+    Модель измеримых свойств товара.
+    Поле measure можно определить через предопределенный список единиц измерения
+    """
+    product = models.ForeignKey(Product)
+    property = models.ForeignKey(Property)
+    value = models.IntegerField(verbose_name='Значение свойства')
