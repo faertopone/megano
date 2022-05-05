cf60eb2c2db427f33f90a64dc0a0c6a3de37c14a
IgorSemenov70 <87079215+IgorSemenov70@users.noreply.github.com>
2022-05-05 21:13:07 +0700

    add template services

---
+++ services/cart.py
@@ -0,0 +1,16 @@
+class Cart:
+    """ Сервисы для работы с корзиной товаров """
+    def get_products_in_cart(self):
+        pass
+
+    def add(self, product):
+        pass
+
+    def remove(self, product):
+        pass
+
+    def total_cost(self):
+        pass
+
+    def get_number_of_products(self):
+        pass
