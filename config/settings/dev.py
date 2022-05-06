50eb087a67c1f49bcb79194acce4a588476fd3cf
Владимир Салтыков <vowatchka@mail.ru>
2022-05-06 06:27:04 +0000

    Feature/top menu and footer

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import *  # noqa: F403
 
 DEBUG = True
 
@@ -31,14 +31,14 @@ LOGGING = {
     },
 }
 
-INSTALLED_APPS.append('debug_toolbar')
-INSTALLED_APPS.append('django_extensions')
+INSTALLED_APPS.append('debug_toolbar')  # noqa: F405
+INSTALLED_APPS.append('django_extensions')  # noqa: F405
 
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')  # noqa: F405
 
 try:
-    INTERNAL_IPS.append('127.0.0.1')
-except:
+    INTERNAL_IPS.append('127.0.0.1')  # noqa: F405
+except Exception:
     INTERNAL_IPS = [
         '127.0.0.1',
     ]
