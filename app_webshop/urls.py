cbc3614f85e27dabf7f0f7db6e6853cf31ca678c
pavlove <pavlove75@mail.ru>
2022-05-09 22:06:23 +0300

    add services

--- app_webshop/urls.py
+++ app_webshop/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import Index
+from .services.views import HistoryView, ProductComment
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('historyview', HistoryView.as_view(), name='history view'),
+    path('product/<int:pk>/comment', ProductComment.as_view(), name='product comment'),
 ]
