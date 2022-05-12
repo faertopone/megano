e9c465dba4d60f4d9729377d4c1d95fd45a60d72
IgorSemenov70 <87079215+IgorSemenov70@users.noreply.github.com>
2022-05-12 23:24:38 +0700

    перенёс своё приложение goods в products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+
+
+class GoodsView(View):
+    # TODO здесь предполагаю будет реализация управления корзиной, просмотр(get)
+    #  добавление(post), удаление(delete) и т.д
+    pass
+
+class PaymentView(View):
+    # TODO для сервиса оплата такое предположение что вообще отдельное приложение необходимо
+    pass
+
+class DiscountView(View):
+    # TODO вообще предполагаю вот скидки на товары урлы и вьюхи не нужны,
+    #  так как скидка возможно будет вычисляемым полем если она есть,
+    #  и автоматически применяться к товарам магазина
+    pass
+
