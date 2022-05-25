18fc90fcff58c77e3bb1fe87271b136e33fbd712
nikolai <nikolaizinchenko94@gmail.com>
2022-05-25 08:53:17 +0300

    fix - verbose name с маленькой буквы

--- accounts/models.py
+++ accounts/models.py
@@ -6,33 +6,33 @@ from django.utils.translation import gettext_lazy as _
 
 class Client(models.Model):
     """Основная модель пользователя"""
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        verbose_name=_("Пользователь")
+        verbose_name=_("пользователь")
     )
-    postcode = models.IntegerField(_("Почтовый индекс"))
-    photo = models.ImageField(_("Фотография"), upload_to='accounts/', null=True)
+    postcode = models.IntegerField(_("почтовый индекс"))
+    photo = models.ImageField(_("фотография"), upload_to='accounts/', null=True)
     phoneNumberRegex = RegexValidator(
         regex=r"^\+?7?\d{8,15}$",
         message='Введите корректный номер, без пробелов (+79999999999)'
     )
     phone = models.CharField(
-        'Контактный номер',
+        'контактный номер',
         validators=[phoneNumberRegex],
         max_length=16,
     )
-    city = models.CharField(_('Город'), max_length=256)
-    street = models.CharField(_("Улица"), max_length=256)
-    house_number = models.IntegerField(_("Номер дома"))
-    apartment_number = models.IntegerField(_("Номер квартиры"))
+    city = models.CharField(_('город'), max_length=256)
+    street = models.CharField(_("улица"), max_length=256)
+    house_number = models.IntegerField(_("номер дома"))
+    apartment_number = models.IntegerField(_("номер квартиры"))
     spent_money = models.DecimalField(
-        _("Потратил денег"),
+        _("потратил денег"),
         max_digits=9,
         decimal_places=2,
         default=0
     )
-    is_seller = models.BooleanField(_("Продавец"), default=False)
+    is_seller = models.BooleanField(_("продавец"), default=False)
 
     class Meta:
         verbose_name = 'клиент'
