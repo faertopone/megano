a5d8019769878e8578676161002d21ffe8c05d8e
sergey_klimov <s.klimov@bk.ru>
2022-07-17 13:02:47 +0700

    Добавлено название задачи

--- orders/tasks.py
+++ orders/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from orders.services import pay_order
 
 
-@shared_task
+@shared_task(name="Оплата заказов")
 def pay_order_task(id_order, visa_number):
     pay_order(id_order, visa_number)
 
