c1153d09d4326934109e384c80f4c955f5d6c4ab
pavlove <pavlove75@mail.ru>
2022-08-01 22:24:08 +0300

    товары на главной странице, кнопка "к сравнению"

--- app_webshop/views.py
+++ app_webshop/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 from banners.models import Banners
 from banners.services import get_banners
+from shops.models import ShopProduct
 
 
 class Index(ListView):
@@ -10,3 +11,12 @@ class Index(ListView):
 
     def get_queryset(self):
         return get_banners()
+
+    def get(self, *args, **kwargs):
+        self.extra_context = dict()
+        self.extra_context['products'] = ShopProduct.objects.select_related(
+            "shop", "product", "promotion"
+        ).all()
+
+        return super().get(*args, **kwargs)
+
