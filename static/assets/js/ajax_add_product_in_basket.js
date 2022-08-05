d7626e2a2f7048cee43e069d0d83181b7c945bf2
Сергей Колыхалов <sergey-kolyhalov@rambler.ru>
2022-08-06 00:50:26 +0300

    - вынес скрипт "добавления товара в корзину" в js файл. - добавил работоспособность кнопки добавить в корзину на странице категории товаров (где фильтр) - добавил на js немного понятности, что кнопка нажата

---
+++ static/assets/js/ajax_add_product_in_basket.js
@@ -0,0 +1,55 @@
+$(document).ready(function (){
+  let add_button = $('#add-button')
+  let url_path ='/basket/add/'
+  let crsf = ($('input[name="csrfmiddlewaretoken"]').val())
+  let all_add_button = $('.add-button')
+        add_button.click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: url_path,
+            data: {
+                product_id: $('#add-button').val(),
+                product_qty: $('#select').val(),
+                csrfmiddlewaretoken: add_button.attr('data-csrf'),
+                action: 'add'
+            },
+            success: function (json) {
+                document.getElementById('basket-qty').innerHTML = json.qty;
+                document.getElementById('h-subtotal').innerHTML = json.subtotal;
+                // document.getElementById('subtotal').innerHTML = json.subtotal;
+            },
+            error: function (xhr, errmsg, err) {
+            }
+        });
+    })
+
+    all_add_button.click(function (e) {
+        e.preventDefault();
+        var prodid = $(this).attr('data-value');
+        console.log(prodid)
+        var shop_prod_id = $(this).attr('data-shop');
+        console.log(shop_prod_id)
+        console.log(crsf)
+        $.ajax({
+            type: 'POST',
+            url: url_path,
+            data: {
+                product_id: prodid,
+                product_qty: 1,
+                shop_product_id: shop_prod_id,
+                csrfmiddlewaretoken: crsf,
+                action: 'add'
+            },
+            success: function (json) {
+                document.getElementById('basket-qty').innerHTML = json.qty;
+                document.getElementById('h-subtotal').innerHTML = json.subtotal;
+                // document.getElementById('subtotal').innerHTML = json.subtotal;
+            },
+            error: function (xhr, errmsg, err) {
+            }
+        });
+    })
+
+})
--- static/assets/js/services_add_shop.js
+++ static/assets/js/services_add_shop.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-  let btn_card = $('#add-button')
+  let btn_card = $('#add-button, .add-button')
   let btn_card_text = $('#add-button .btn-content')
   // если эта кнопка есть на странице
   if (btn_card){
     btn_card.click(function (){
-      btn_card_text.text('Товар добавлен в корзину')
-      $(this).css('background-color', 'green')
+      btn_card_text.text('Товар добавлен')
+      $(this).css('background-color', '#1f7eff')
     })
   }
 
