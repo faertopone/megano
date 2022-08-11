fd5dc5bba6fca7a097eab0bcd1652e100569ad69
Сергей Колыхалов <sergey-kolyhalov@rambler.ru>
2022-08-11 18:20:17 +0300

    -добавил фиксутру Товар дня + убрал лишние проверки

--- app_webshop/views.py
+++ app_webshop/views.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.views.generic import ListView
 from banners.models import Banners
 from banners.services import get_banners
+from products.models import Product
 from promotions.models import PromotionsShowProduct
 from shops.models import ShopProduct
 
